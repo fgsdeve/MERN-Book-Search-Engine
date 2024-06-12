f3041b8c5854a31e8d8124e2e110ed4d5e70cec3
Francdeve <156461955+fgsdeve@users.noreply.github.com>
2024-06-12 17:31:10 -0500

    added dist folder to the client server and changed the line 29 and 33 from build to dist

--- server/server.js
+++ server/server.js
@@ -26,11 +26,11 @@ async function startApolloServer() {
   app.use("/graphql", expressMiddleware(server, {context: authMiddleware}));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 
   db.once('open', () => {
